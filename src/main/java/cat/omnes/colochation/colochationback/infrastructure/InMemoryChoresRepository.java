45ba0c850b74a1fa5cbbfb8bdf78041798a310bc
Nigth <omnes.theo@gmail.com>
2023-07-25 16:19:47 +0200

    transform status to boolean

--- src/main/java/cat/omnes/colochation/colochationback/infrastructure/InMemoryChoresRepository.java
+++ src/main/java/cat/omnes/colochation/colochationback/infrastructure/InMemoryChoresRepository.java
@@ -15,8 +15,8 @@ public class InMemoryChoresRepository implements ChoresRepository {
     private final Map<UUID, Chore> chores = new HashMap<>(
             Map.of(
                     UUID.randomUUID(), new Chore("Faire la vaisselle", "to_do", "Nathan"),
-                    UUID.randomUUID(), new Chore("Nettoyer la salle de bain", "in_progress", "Nathan"),
-                    UUID.randomUUID(), new Chore("Passer l'aspirateur", "done", "Theo")
+                    UUID.randomUUID(), new Chore("Nettoyer la salle de bain", "to_do", "Nathan"),
+                    UUID.randomUUID(), new Chore("Passer l'aspirateur", "to_do", "Theo")
             )
     );
 
